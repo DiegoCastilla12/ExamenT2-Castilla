e330f572d88d2667791cc811e8be612702d96819
SUITE <SUITE@SUITESB604-ST06>
2025-06-22 15:39:29 -0500

    Proyecto iniciado - Castilla Diego

---
+++ src/main/java/com/ExamenT2/Lp2T2CastillaDiegoApplication.java
@@ -0,0 +1,13 @@
+package com.ExamenT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2CastillaDiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2CastillaDiegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ExamenT2/Lp2T2CastillaDiegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExamenT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2CastillaDiegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
